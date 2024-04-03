dcd1ea53afbe1b2f471723b7666830503dd77b24
Apolo Rae <aresjae777@gmail.com>
2024-04-03 18:04:57 -0400

    feat: update app

--- src/app.js
+++ src/app.js
@@ -7,3 +7,10 @@ const metricsRoutes = require('./routes/metrics');
 
 const app = express();
 
+app.use(cors());
+app.use(express.json({limit:'2mb'}));
+
+app.get('/health',(_req,res) => {
+  res.json({status:'ok',service:'release-control'});
+});
+
