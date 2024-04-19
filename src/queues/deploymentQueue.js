d3076a636cc4aba55ccdcb26743d52d928ea6247
Apolo Rae <aresjae777@gmail.com>
2024-04-19 09:07:06 -0400

    feat: implement deploymentQueue

--- src/queues/deploymentQueue.js
+++ src/queues/deploymentQueue.js
@@ -1,2 +1,14 @@
 const { Queue } = require('bullmq');
 
+let deploymentQueue;
+
+function getDeploymentQueue() {
+  if (!deploymentQueue) {
+    const redis = require('../redis');
+    deploymentQueue = new Queue('deployment-jobs', {
+      connection: redis
+    });
+  }
+  return deploymentQueue;
+}
+
