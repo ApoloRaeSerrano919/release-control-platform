606fa778629edb25159bb49cdcbcfe93436cefe8
Apolo Rae <aresjae777@gmail.com>
2024-05-14 09:18:43 -0400

    fix services

--- src/routes/services.js
+++ src/routes/services.js
@@ -43,3 +43,8 @@ router.post('/', async (req,res) => {
 
     res.status(201).json(service);
   } catch (error) {
+    res.status(400).json({error:error.message});
+  }
+});
+
+module.exports = router;
