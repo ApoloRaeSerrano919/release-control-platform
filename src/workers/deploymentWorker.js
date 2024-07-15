b9fdd1dcd30765389b15b5eafe4d02e44bc01b0c
Apolo Rae <aresjae777@gmail.com>
2024-07-15 19:34:24 -0400

    feat: add logger

--- src/workers/deploymentWorker.js
+++ src/workers/deploymentWorker.js
@@ -1,3 +1,5 @@
 const { Worker } = require('bullmq');
 const redis = require('../redis');
 const pool = require('../db');
+const logger = require('../logger');
+const { acquireLock,releaseLock } = require('../services/lockService');
