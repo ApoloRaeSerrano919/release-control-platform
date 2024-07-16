3ec5e7504c8ea7ce43072b4652997e64e761d7e0
Apolo Rae <aresjae777@gmail.com>
2024-07-15 20:04:06 -0400

    feat: update deploymentWorker

--- src/workers/deploymentWorker.js
+++ src/workers/deploymentWorker.js
@@ -3,3 +3,7 @@ const redis = require('../redis');
 const pool = require('../db');
 const logger = require('../logger');
 const { acquireLock,releaseLock } = require('../services/lockService');
+const { addEvent } = require('../services/eventService');
+const { runHealthChecks } = require('../services/healthService');
+const { deployArtifact,rollbackArtifact } = require('../services/deploymentAdapter');
+
