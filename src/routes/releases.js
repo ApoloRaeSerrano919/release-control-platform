bfbb78ea44dfaf731da8f93a2f377a7a242676a7
Apolo Rae <aresjae777@gmail.com>
2024-08-05 18:32:21 -0400

    feat(routes): update releases

--- src/routes/releases.js
+++ src/routes/releases.js
@@ -2,3 +2,9 @@ const express = require('express');
 const {z} = require('zod');
 const pool = require('../db');
 const {createRelease} = require('../services/releaseService');
+const {
+  queueDeployment,
+  approveProduction,
+  queueRollback
+} = require('../services/orchestrationService');
+
