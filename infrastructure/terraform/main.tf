e3502f232cc7d1d67b6027d46f90fe28cd39714f
Apolo Rae <aresjae777@gmail.com>
2024-09-16 14:45:01 -0400

    more main

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -21,3 +21,8 @@ resource "aws_ecr_repository" "release_control" {
   }
 }
 
+resource "aws_cloudwatch_log_group" "api" {
+  name              = "/${var.project_name}/api"
+  retention_in_days = 14
+}
+
