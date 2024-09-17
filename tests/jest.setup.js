2c1fab1f7e84154025742d0177fc2c56e644785f
Apolo Rae <aresjae777@gmail.com>
2024-09-17 12:20:09 -0400

    test: add queuePath
    Implement queuePath in jest.setup.

--- tests/jest.setup.js
+++ tests/jest.setup.js
@@ -1,1 +1,10 @@
 afterAll(async () => {
+  const queuePath = require.resolve('../src/queues/deploymentQueue');
+  const redisPath = require.resolve('../src/redis');
+  const dbPath = require.resolve('../src/db');
+
+  if (require.cache[queuePath]) {
+    const { closeDeploymentQueue } = require('../src/queues/deploymentQueue');
+    await closeDeploymentQueue();
+  }
+
